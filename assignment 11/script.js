cf410c2c668c0edf711e612d95bc480c93b2827d
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-07-11 08:39:52 -0700

    Add files via upload

---
+++ assignment 11/script.js
@@ -0,0 +1,76 @@
+// let char = prompt("Enter a character:");
+
+// if (char.length !== 1) {
+//     alert("Invalid input. Please enter a single character.");
+// } else {
+//     if (char >= '0' && char <= '9') {
+//         alert("The character is a number.");
+//     } else if (char >= 'A' && char <= 'Z') {
+//         alert("The character is an uppercase letter.");
+//     } else if (char >= 'a' && char <= 'z') {
+//         alert("The character is a lowercase letter.");
+//     } else {
+//         alert("The character is neither a number, uppercase letter, nor lowercase letter.");
+//     }
+// }
+// let num1 = +prompt("Enter the first integer:");
+// let num2 = +prompt("Enter the second integer:");
+// if(num1 > num2){
+//     console.log("The first integer (" + num1 + ") is larger.");
+// }
+// else if (num1 < num2){
+//     console.log("The second integer " + num2 + " is larger.");
+// }
+// else {
+//     console.log("Both are equal ");
+// // }
+// let num3 = prompt("Enter any number");
+// if(num3  > 0 ){
+//     console.log("This number is positive");
+// }
+// else if(num3 < 0){
+//     console.log("This number is negative");
+// }
+// else{
+//     console.log("The number is zero");
+// // }
+// let char = prompt("Enter a character:").toLowerCase();
+
+// if (char.length !== 1) {
+//     console.log("Invalid input. Please enter a single character.");
+// } else {
+//     let isVowel = char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u';
+//     console.log(isVowel);
+// // }
+// let password1 = "eshal123@";
+// let _password = prompt("Enter your password")
+// let $password = prompt("Please confirm your password");
+// if (_password && $password === "eshal123@"){
+//     console.log("Correct");
+// }
+// else{
+//     console.log("Incorrect");
+// }
+// var hour = 13;
+// var greeting;
+
+// if (hour < 12) {
+//     greeting = "Good morning";
+// } else {
+//     greeting = "Good afternoon";
+// }
+
+// console.log(greeting); // Outputs: "Good afternoon" (since hour is 13, which is not less than 12)
+// let time = prompt("Enter the time ");
+// if (time >= "00:00" && time < "12:00") {
+//     document.write("Good morning")
+// }
+// else if (time >= "12:00" && time < "17:00"  ){
+//     document.write("Good afternoon")
+// }
+// else if (time >= "17:00" && time < "21:00"  ){
+//     document.write("Good evening")
+// }
+// else if (time >= "21:00" && time < "23:50"  ){
+//     document.write("Good night")
+// }
